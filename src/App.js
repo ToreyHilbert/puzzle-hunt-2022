08c3fd533be06844ac3119e3295bfc9b58fd1147
Torey Hilbert <thilbert271@gmail.com>
2022-04-05 22:04:02 -0400

    Changed get-team-data useEffect to be within IIFE

--- src/App.js
+++ src/App.js
@@ -9,13 +9,15 @@ import { SubmissionForm } from './components/SubmissionForm.js';
 export const App = () => {
   const [teamData, setTeamData] = useState([])
 
-  useEffect(async () => {
-    try {
-      const response = await axios.get("/.netlify/functions/get-team-data")
-      setTeamData(response.data)
-    } catch (error) {
-      console.log(error.toJSON())
-    }
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get("/.netlify/functions/get-team-data")
+        setTeamData(response.data)
+      } catch (error) {
+        console.log(error.toJSON())
+      }
+    })()
   }, [])
 
   return (
